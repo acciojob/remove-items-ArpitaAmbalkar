904f1ba90b7d1e3cc745cfe4253c90353622bbb6
ArpitaAmbalkar <arpitaambalkar26@gmail.com>
2025-09-26 19:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let select=document.getElementById("colorSelect");
+let btn=document.getElementById("removeBtn");
+
+btn.addEventListener("click",()=>{
+	let index=select.selectedIndex;
+	if(index >= 0){ 
+        select.remove(index);
+	}
+	
+})
